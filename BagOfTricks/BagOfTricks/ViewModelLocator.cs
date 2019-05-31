1ad083da34ebbf9e10c3f593114bc9ba3c7f6241
elwood <c-ts@gmx.de>
2019-05-31 20:55:53 +0200

    Basic structure

---
+++ BagOfTricks/BagOfTricks/ViewModelLocator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Practices.ServiceLocation;
+using GalaSoft.MvvmLight.Ioc;
+
+namespace BagOfTricks
+{
+    public class ViewModelLocator
+    {
+        public ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+
+            // Register types
+
+            // Register viewmodels
+            SimpleIoc.Default.Register<ViewModels.BackgroundMusicViewModel>();
+            SimpleIoc.Default.Register<ViewModels.SoundEffectsViewModel>();
+        }
+
+        public ViewModels.BackgroundMusicViewModel BackgroundMusic
+        {
+            get
+            {
+                return SimpleIoc.Default.GetInstance<ViewModels.BackgroundMusicViewModel>();
+            }
+        }
+
+        public ViewModels.SoundEffectsViewModel SoundEffects
+        {
+            get
+            {
+                return SimpleIoc.Default.GetInstance<ViewModels.SoundEffectsViewModel>();
+            }
+        }
+    }
+}
---
+++ BagOfTricks/BagOfTricks/ViewModels/BackgroundMusicViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+using GalaSoft.MvvmLight.Ioc;
+
+namespace BagOfTricks.ViewModels
+{
+    public class BackgroundMusicViewModel : ViewModelBase
+    {
+        public BackgroundMusicViewModel()
+        {
+            
+        }
+    }
+}
